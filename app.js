95caf30ca109f153b3552f8d8de0a8159e4dcd0d
Waleed Waseem <waleed.waseem@careaxiom.com>
2018-11-23 12:43:15 +0500

    fetching the ipaddress of a local system.

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(function(req, res, next) {
+    var ip = req.headers['x-forwarded-for'] || 
+         req.connection.remoteAddress || 
+         req.socket.remoteAddress ||
+         (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    next();
+    
+});
+
+app.get('/', (req, res) => res.send('Heyy'));     
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
